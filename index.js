1d1b2b165c8959026d50f120540bf6c6bcb85541
Emmanuel-FSE <emmanuelmutisya254@gmail.com>
2022-12-06 23:57:10 +0300

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Add your code here
+function submitData(name, email){
+   const userData = {
+        name: name,
+        email:email
+    };
+    // console.log(userData);
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify(userData),
+    })
+    .then(res => res.json())
+    .then(data => {
+        const pTag = document.createElement("p");
+        pTag.textContent = data.id;
+        document.querySelector("body").appendChild(pTag);
+    })
+    .catch(error => {
+        const pTag = document.createElement("p");
+        pTag.textContent = error.message;
+        document.querySelector("body").appendChild(pTag);
+    })
+}
+
+submitData("mkamba", "mkamba@gmail.com")
